5f47f98924f3694c84a6fe8558da4c9d2ba0785f
Ilya Radchenko <ilya@burstcreations.com>
2016-09-13 09:57:10 -0400

    Convert to app folder

---
+++ app/orm-adapters/knex.js
@@ -0,0 +1,55 @@
+import { ORMAdapter } from 'denali';
+
+export default class KnexAdapter extends ORMAdapter {
+  static find(type, query, options) {
+
+  }
+
+  static createRecord(type, data, options) {
+
+  }
+
+  static idFor(record) {
+
+  }
+
+  static getAttribute(record, property) {
+
+  }
+
+  static setAttribute(record, property) {
+
+  }
+
+  static deleteAttribute(record, property) {
+
+  }
+
+  static getRelationship(record, relationship) {
+
+  }
+
+  static setRelationship(record, relationship) {
+
+  }
+
+  static defineRelationships(record, relationship) {
+
+  }
+
+  static deleteRelationship(record, relationship) {
+
+  }
+
+  static saveRecord(record, options) {
+
+  }
+
+  static deleteRecord(record, options) {
+
+  }
+
+  static define(Model) {
+
+  }
+}
---
+++ config/initializers/knex.js
@@ -0,0 +1,59 @@
+import { resolve, fromNode } from 'bluebird';
+import mapValues from 'lodash/mapValues';
+import forEach from 'lodash/forEach';
+import knex from 'knex';
+
+export default {
+  name: 'knex',
+
+  initialize({ container, config, logger }) {
+    let adapter = container.lookup('orm-adapter:knex');
+
+    if (config.ormAdapter === 'knex') {
+      container.register('orm-adapter:application', adapter);
+    }
+
+    debugger;
+
+    return resolve(() => {
+      debugger;
+      return knex(config.knex);
+    }).then((db) => {
+      debugger;
+      adapter.db = db;
+
+      let models = container.lookupAll('model');
+      let adapterModels = mapValues(models, (Model) => {
+        if (Model.hasOwnProperty('abstract') && Model.abstract) {
+          return;
+        }
+        return adapter.define(Model);
+      });
+
+      adapter.adapterModels = adapterModels;
+      adapter.models = models;
+
+      forEach(adapterModels, (AdapterModel, type) => {
+        adapter.defineRelationships(models[type], AdapterModel);
+      });
+
+      // Create tables for each model if they don't exist
+      return createTables(db, adapterModels);
+    }).catch((error) => {
+      logger.error('Error initializing the knex adapter or database connection:');
+      logger.error(error.stack);
+    });
+  }
+};
+
+function createTables(db, models) {
+  return db.schema.table('community_configs', function (table) {
+    table.integer('version');
+    table.json('settings');
+    table.text('piwik_site_id');
+    table.text('cartodb_api_key');
+    table.text('cartodb_account');
+    table.text('subtitle');
+    table.dropForeign('config_id');
+  });
+}
